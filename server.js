35c92fd2e6dbba7fe49499a269599f9a315b7a9c
Christopher Adegoke <christopheradegoke4@gmail.com>
2025-12-09 18:03:08 +0100

    learning redis

---
+++ server.js
@@ -0,0 +1,21 @@
+import redis from "redis"
+
+const client = redis.createClient({
+    host: "localhost",
+    port: 6379
+})
+
+// event listener
+client.on("error", (err) => { console.log("Redis client error.", err) })
+
+async function testRedisConnection() {
+    try {
+        await client.connect()
+        console.log("Connected to Redis.");
+    }
+    catch (err) {
+        console.error(err)
+    } finally {
+        await client.quit()
+    }
+}
