daa732e47aab89504ba006d71c7b7bddc830a6ed
Christopher Adegoke <christopheradegoke4@gmail.com>
2025-12-10 09:09:11 +0100

    learning the set, get,incr and decr methods in redis

--- server.js
+++ server.js
@@ -12,6 +12,29 @@ async function testRedisConnection() {
     try {
         await client.connect()
         console.log("Connected to Redis.");
+
+        await client.set("name", "chris")
+
+        const extractValue = await client.get("key")
+        console.log(extractValue);
+
+        const deleteCount = await client.del("name")
+        console.log(deleteCount);
+
+        const extractedValues = await client.get("name")
+        console.log(extractedValues);
+
+        await client.set("count", "100")
+        const incrementCount = await client.incr("count")
+
+        console.log(incrementCount);
+
+        const decrementCount = await client.decr("count")
+        console.log(decrementCount);
+
+
+
+
     }
     catch (err) {
         console.error(err)
